9b321987ee3a3e326d116e47b87952c86fe41a9a
root <root@DESKTOP-9PQF3MH.localdomain>
2020-05-28 14:27:07 +0530

    Added: Add Uc2,UC3,UC5 & UC6

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,28 +1,122 @@
-#!/bin/bash -x
-flip()
-{
-    if [[ $((RANDOM%2)) == 0 ]]
-    then
-        echo "heads"
-    else
-        echo "tails"
-    fi
-}
-
-declare -A singlet
-singlet[heads]=0
-singlet[tails]=0
+#!/bin/bash
+cntH=0;
+cntT=0;
+declare -A singlets
+declare -A double
+declare -A triplet
 for ((i=0;i<10;i++))
 do
-    if [[ $(flip) == "heads" ]] 
-    then 
-        
-        singlet[heads]=$((${singlet[heads]}+1))
-    else
-        singlet[tails]=$((${singlet[tails]}+1))
-    fi
+        n=$(($RANDOM%2))
+        if [[ $n -eq 0 ]]
+        then
+                singlets[$i]="H"
+                ((cntH++))
+        else
+                singlets[$i]="T"
+                ((cntT++))
+        fi
 done
-echo ${singlet[@]}
-echo "heads percent is $(echo "scale=2;(${singlet[heads]}/10)*100" | bc) and tails percentage is  $(echo "scale=2;(${singlet[tails]}/10)*100" | bc)"
+headPercentage=`echo "scale=2;$cntH/10*100" | bc`
+tailPercentage=`echo "scale=2;$cntT/10*100" | bc`
+if (( $(echo "$headPercentage>$tailPercentage" | bc -l) ))
+then
+        echo "In singlet Combination,Heads wins by $headPercentage percentage"
+elif (( $(echo "$tailPercentage>$headPercentage" | bc -l) ))
+then
+        echo "In singlet Combination,Tails wins by $tailPercentage percentage"
+else
+        echo "It is a tie"
+fi
+cntH=0;
+cntT=0;
+for ((i=0;i<10;i++))
+do
 
+        n1=$(($RANDOM%2))
+        if [[ $n -eq 0 && $n1 -eq 0 ]]
+        then
+                double[$i]="HH"
+                ((cntH++))
+        elif [[  $n -eq 1 && $n1 -eq 1 ]]
+        then
+                double[$i]="TT"
+                ((cntT++))
+        elif [[  $n -eq 0 && $n1 -eq 1 ]]
+        then
+                double[$i]="HT"
+                ((cntH++))
+                ((cntT++))
+        else
+                double[$i]="TH"
+                ((cntT++))
+                ((cntH++))
+        fi
+done
+headPercentage=`echo "scale=2;$cntH/10*100" | bc`
+tailPercentage=`echo "scale=2;$cntT/10*100" | bc`
+if (( $(echo "$headPercentage>$tailPercentage" | bc -l) ))
+then
+                echo "In Doublet,Heads wins by $headPercentage percentage"
+elif (( $(echo "$tailPercentage>$headPercentage" | bc -l) ))
+then
+                echo "In Doublet,Tails wins by $tailPercentage percentage"
+else
+                echo "It is a tie"
+fi
+cntH=0
+cntT=0
+for ((i=0;i<10;i++))
+do
+        n2=$(($RANDOM%2))
+        if [[ $n -eq 0 && $n1 -eq 0 && $n2 -eq 0 ]]
+        then
+                triplet[$i]="HHH"
+                ((cntH++))
+        elif [[  $n -eq 1 && $n1 -eq 1 && $n -eq 1 ]]
+        then
+                triplet[$i]="TTT"
+                ((cntT++))
+        elif [[  $n -eq 0 && $n1 -eq 0 && $n -eq 1 ]]
+        then
+                triplet[$i]="HHT"
+                ((cntH++))
+                ((cntT++))
+        elif [[  $n -eq 1 && $n1 -eq 0 && $n -eq 0 ]]
+        then
+                triplet[$i]="THH"
+                ((cntH++))
+                ((cntT++))
+        elif [[  $n -eq 0 && $n1 -eq 1 && $n -eq 1 ]]
+        then
+                 triplet[$i]="HTT"
+                ((cntT++))
+                ((cntH++))
+        elif [[  $n -eq 1 && $n1 -eq 1 && $n -eq 0 ]]
+        then
+                 triplet[$i]="TTH"
+                ((cntT++))
+                ((cntH++))
+        elif [[  $n -eq 1 && $n1 -eq 0 && $n -eq 1 ]]
+        then
+                 triplet[$i]="THT"
+                ((cntT++))
+                ((cntH++))
+        else
+                 triplet[$i]="HTH"
+                ((cntT++))
+                ((cntH++))
+        fi
+done
+
+headPercentage=`echo "scale=2;$cntH/10*100" | bc`
+tailPercentage=`echo "scale=2;$cntT/10*100" | bc`
+if (( $(echo "$headPercentage>$tailPercentage" | bc -l) ))
+then
+                echo "In Triplet,Heads wins by $headPercentage percentage"
+elif (( $(echo "$tailPercentage>$headPercentage" | bc -l) ))
+then
+                echo "In Triplet,Tails wins by $tailPercentage percentage"
+else
+                echo "It is a tie"
+fi
 
