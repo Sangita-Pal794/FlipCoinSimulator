676b23eb270b60aefa5dd276ff4c0b210ef9c6da
root <root@DESKTOP-9PQF3MH.localdomain>
2020-05-28 14:15:42 +0530

    Added:flipping a coin store the Singlet Combination in a Dictionary.

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -9,3 +9,20 @@ flip()
     fi
 }
 
+declare -A singlet
+singlet[heads]=0
+singlet[tails]=0
+for ((i=0;i<10;i++))
+do
+    if [[ $(flip) == "heads" ]] 
+    then 
+        
+        singlet[heads]=$((${singlet[heads]}+1))
+    else
+        singlet[tails]=$((${singlet[tails]}+1))
+    fi
+done
+echo ${singlet[@]}
+echo "heads percent is $(echo "scale=2;(${singlet[heads]}/10)*100" | bc) and tails percentage is  $(echo "scale=2;(${singlet[tails]}/10)*100" | bc)"
+
+
